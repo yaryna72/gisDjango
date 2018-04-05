3ec70f10780c0942bae0968d674c6f244995d5d1
yaryna <yaryna.kruchenyuk@gmail.com>
2018-04-05 13:50:22 +0300

    Made articles, about, static file

--- gis/models.py
+++ gis/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+SHORT_TEXT_LEN=700
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +12,9 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_short_text(self):
+        if len(self.text) > SHORT_TEXT_LEN:
+            return self.text[:SHORT_TEXT_LEN]
+        else:
+            return self.text
--- gis/views.py
+++ gis/views.py
@@ -1,9 +1,27 @@
 
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from gis.models import Articles
 # Create your views here.
 def home(request):
-    # return HttpResponse('Hello world')
-    return render(request, 'gis/home.html')
+    # return HttpResponse("Hello world")
+    # return render(request, 'gis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'gis/home.html', context)
 
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'gis/article.html', {'article': article})
+
+
+def about(request):
+    return render(request, 'gis/about.html')
+
+
+def mymap(request):
+    return render(request, 'gis/map.html')

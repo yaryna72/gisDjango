e7cc9c50b67dcaae97b905b00560e1cc75f5a5b5
yaryna <yaryna.kruchenyuk@gmail.com>
2018-04-05 12:33:14 +0300

    Home functions were created

--- gis/views.py
+++ gis/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+def home(request):
+    # return HttpResponse('Hello world')
+    return render(request, 'gis/home.html')
+
